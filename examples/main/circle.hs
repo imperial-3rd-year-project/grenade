8441e93aebc210018a920f148b01344a4bc443d7
Aporva Varshney <av918@ic.ac.uk>
2020-11-08 23:35:14 +0000

    Add tutorial-style comments to the circle example

--- examples/main/circle.hs
+++ examples/main/circle.hs
@@ -11,7 +11,23 @@ import qualified Numeric.LinearAlgebra.Static as SA
 
 import           Grenade
 
+{--
+  Learning circles
+  ================
+    This simple example shows how to create a
+    network which learns the shape of a circle.
+--}
 
+{--
+  The Network type
+  ================
+    For this simple example, we will use a network consisting of
+    fully connected layers. There are two hidden layers with 40 neurons
+    and 10 neurons respectively. We use the tanh, relu and logit 
+    activation functions.
+    Given an (x, y) coordinate, the output is the probability the 
+    coordinate is inside the circle.
+--}
 type FFNet 
   = Network 
     '[ FullyConnected 2 40, Tanh, 
@@ -24,7 +40,9 @@ type FFNet
        'D1 1, 'D1 1    -- Logit fully connected
      ]
 
--- A simple function for displaying the circle
+{--
+  A helper function for displaying the circles.
+--}
 drawCircle :: FFNet -> IO ()
 drawCircle network = do
     let testIns = [ [ (x,y)  | x <- [0..50] ] | y <- [0..20] ]
@@ -43,7 +61,12 @@ drawCircle network = do
 
     renderCoord (x, y) = (render . normx) $ runNet network (S1D $ SA.vector [x / 25 - 1, y / 10 - 1])
 
--- This is the function we will attempt to learn
+{--
+  This is the function we are attempting to learn. 
+  Given a vector (x, y), we check if the distance from the origin
+  is less than a radius of 0.5, and return 1 if the coordinate is
+  inside the circle and 0 otherwise.
+--}
 circle :: S ('D1 2) -> S ('D1 1)
 circle (S1D v) =
   if v `inCircle` (fromRational 0, 0.5)
@@ -54,18 +77,32 @@ circle (S1D v) =
     u `inCircle` (o, r) = SA.norm_2 (u - o) <= r
     
 
+{--
+  Training and Execution
+  ================
+    Now we train our network and run it.
+--}
 main :: IO ()
 main = do
+    -- Train on 100000 examples, randomly generated with a uniform 
+    -- distribution.
     let n = 100000
     inps <- replicateM n $ do
       s  <- getRandom
       return $ S1D $ SA.randomVector s SA.Uniform * 2 - 1
+    -- For our network, we will use stochastic gradient descent with a batch
+    -- size of 1. 
     let options = TrainingOptions { optimizer      = OptSGD 0.01 0.9 0.0001
                                   , batchSize      = 1
                                   , validationFreq = 1
                                   , verbose        = Full 
-                                  , metrics        = []
+                                  , metrics        = [Quadratic]
                                   }
-    -- No validation data
+    -- Now we train the network with the fit function.
+    -- We pass in a list of tuples of inputs and expected outputs,
+    -- and use the quadratic loss function. We train for one epoch.
+    -- There is no validation data, hence we pass in an empty list.
     net <- fit (zip inps (map circle inps)) [] options 1 quadratic'
+
+    -- Now we visualise what our network has learned.
     drawCircle net
