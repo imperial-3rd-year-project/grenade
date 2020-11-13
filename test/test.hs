86522edd5d37a985000d641dd81336a3b627e7eb
Theo Charalambous <tc4618@ic.ac.uk>
2020-11-13 10:19:26 +0000

    Remove old batch test cases

--- test/test.hs
+++ test/test.hs
@@ -9,7 +9,6 @@ import qualified Test.Grenade.Layers.Convolution
 import qualified Test.Grenade.Layers.FullyConnected
 import qualified Test.Grenade.Layers.Nonlinear
 import qualified Test.Grenade.Layers.PadCrop
-import qualified Test.Grenade.Layers.BatchNorm
 
 import qualified Test.Grenade.Layers.Internal.Convolution
 import qualified Test.Grenade.Layers.Internal.Pooling
@@ -32,7 +31,6 @@ main = disorderMain [
     , Test.Grenade.Layers.FullyConnected.tests
     , Test.Grenade.Layers.Nonlinear.tests
     , Test.Grenade.Layers.PadCrop.tests
-    , Test.Grenade.Layers.BatchNorm.tests
 
     , Test.Grenade.Layers.Internal.Convolution.tests
     , Test.Grenade.Layers.Internal.Pooling.tests
