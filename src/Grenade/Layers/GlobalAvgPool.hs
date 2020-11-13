69f3324220e03d2f473c587f8a4e7f7d86d14e46
Theo Charalambous <tc4618@ic.ac.uk>
2020-11-13 15:27:44 +0000

    Fix calculation error in GlobalAvgPool

--- src/Grenade/Layers/GlobalAvgPool.hs
+++ src/Grenade/Layers/GlobalAvgPool.hs
@@ -44,7 +44,11 @@ instance Serialize GlobalAvgPool where
 instance (KnownNat i) => Layer GlobalAvgPool ('D1 i) ('D1 1) where
   type Tape GlobalAvgPool ('D1 i) ('D1 1) = S ('D1 i)
 
-  runForwards _ x = (x, S1D . listToVector $ [nsum x])
+  runForwards _ x = 
+    let n   = fromIntegral $ natVal (Proxy :: Proxy i)
+        avg = nsum x / n
+        vec = listToVector [avg]   
+    in  (x, S1D vec)
 
   runBackwards = undefined
 
@@ -52,9 +56,11 @@ instance (KnownNat i, KnownNat j) => Layer GlobalAvgPool ('D2 i j) ('D2 1 1) whe
   type Tape GlobalAvgPool ('D2 i j) ('D2 1 1) = S ('D2 i j)
 
   runForwards _ x =
-    let y' = [nsum x]
-        y  = LAS.fromList $ y' :: L 1 1
-    in  (x, S2D y)
+    let n   = fromIntegral $ natVal (Proxy :: Proxy i)
+        m   = fromIntegral $ natVal (Proxy :: Proxy j)
+        avg = nsum x / (n * m)
+        mat = LAS.fromList [avg] :: L 1 1
+    in  (x, S2D mat)
 
   runBackwards = undefined
 
@@ -63,8 +69,11 @@ instance (KnownNat i, KnownNat j, KnownNat k) => Layer GlobalAvgPool ('D3 i j k)
   type Tape GlobalAvgPool ('D3 i j k) ('D3 1 1 k) = S ('D3 i j k)
 
   runForwards _ x =
-    let cs = splitChannels x
-        ys = map nsum cs
-    in  (x, S3D . LAS.fromList $ ys )
+    let n   = fromIntegral $ natVal (Proxy :: Proxy i)
+        m   = fromIntegral $ natVal (Proxy :: Proxy j)
+        cs  = splitChannels x
+        ys  = map (\c -> nsum c / (n * m)) cs
+        mat = LAS.fromList ys :: L k 1
+    in  (x, S3D mat)
 
   runBackwards = undefined
