06f0d4a44d2cd4aa854fc406ea7215c2e9fe562e
Jason Xu <ruiyang.xu18@imperial.ac.uk>
2020-12-22 01:03:16 +0000

    Parallel im2col w/ memcpy

--- bench/bench.hs
+++ bench/bench.hs
@@ -41,18 +41,6 @@ main = do
         , bench "im2col 100x100" $ whnf (im2col 10 10 1 1) ((100 >< 100) [1 ..])
         ]
     , bgroup
-        "im2col_new"
-        [ bench "im2col_new 3x4" $ whnf (im2col_new 2 2 1 1) ((3 >< 4) [1 ..])
-        , bench "im2col_new 28x28" $ whnf (im2col_new 5 5 1 1) ((28 >< 28) [1 ..])
-        , bench "im2col_new 100x100" $ whnf (im2col_new 10 10 1 1) ((100 >< 100) [1 ..])
-        ]
-    , bgroup
-        "im2col_memcpy"
-        [ bench "im2col_memcpy 3x4" $ whnf (im2col_memcpy 2 2 1 1) ((3 >< 4) [1 ..])
-        , bench "im2col_memcpy 28x28" $ whnf (im2col_memcpy 5 5 1 1) ((28 >< 28) [1 ..])
-        , bench "im2col_memcpy 100x100" $ whnf (im2col_memcpy 10 10 1 1) ((100 >< 100) [1 ..])
-        ]
-    , bgroup
         "col2im"
         [ bench "col2im 3x4" $ whnf (col2im 2 2 1 1 3 4) ((6 >< 4) [1 ..])
         , bench "col2im 28x28" $ whnf (col2im 5 5 1 1 28 28) ((576 >< 25) [1 ..])
--- cbits/im2col.c
+++ cbits/im2col.c
@@ -11,62 +11,23 @@ void im2col_cpu(const RealNum* data_im, const int channels,
 
   const int channel_size = height * width;
 
-  for (int fitting_height = 0; fitting_height <= (height - kernel_h); fitting_height += stride_h) {
-    for (int fitting_width = 0; fitting_width <= (width - kernel_w); fitting_width += stride_w) {
-      for (int channel = 0; channel < channels; channel++) {
-        for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
-          for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
-            int input_row = fitting_height + kernel_row;
-            int input_col = fitting_width + kernel_col;
-            *(data_col++) = data_im[input_row * width + input_col + channel_size * channel];
-          }
-        }
-      }
-    }
-  }
-}
-
-void im2col_cpu_memcpy(const RealNum* data_im, const int channels,
-    const int height, const int width, const int kernel_h, const int kernel_w,
-    const int stride_h, const int stride_w,
-    RealNum* data_col) {
-
-  const int channel_size = height * width;
-
-  for (int fitting_height = 0; fitting_height <= (height - kernel_h); fitting_height += stride_h) {
-    for (int fitting_width = 0; fitting_width <= (width - kernel_w); fitting_width += stride_w) {
-      for (int channel = 0; channel < channels; channel++) {
-        for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
-          int input_row = fitting_height + kernel_row;
-          memcpy(data_col, data_im + input_row * width + channel_size * channel, sizeof(RealNum) * kernel_w);
-          data_col += kernel_w;
-        }
-      }
-    }
-  }
-}
-
-void im2col_cpu_new(const RealNum* data_im, const int channels,
-    const int height, const int width, const int kernel_h, const int kernel_w,
-    const int stride_h, const int stride_w,
-    RealNum* data_col) {
-
-  const int channel_size = height * width;
-
   const int fitting_height_i_max = (height - kernel_h) / stride_h;
   const int fitting_width_i_max = (width - kernel_w) / stride_w;
   
   const int block_size = (fitting_width_i_max + 1) * channels * kernel_h * kernel_w;
 
+  // TODO: Disable parallel for small matrices
 #pragma omp parallel for
   for (int fitting_height_i = 0; fitting_height_i <= fitting_height_i_max; fitting_height_i++) {
     int fitting_height = fitting_height_i * stride_h;
     RealNum* block_data_col = data_col + fitting_height_i * block_size;
     for (int fitting_width_i = 0; fitting_width_i <= fitting_width_i_max; fitting_width_i++) {
+      int fitting_width = fitting_width_i * stride_w;
       for (int channel = 0; channel < channels; channel++) {
         for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
           int input_row = fitting_height + kernel_row;
-          memcpy(block_data_col, data_im + input_row * width + channel_size * channel, sizeof(RealNum) * kernel_w);
+          const RealNum* cpy_start = &data_im[fitting_width + input_row * width + channel_size * channel];
+          memcpy(block_data_col, cpy_start, sizeof(RealNum) * kernel_w);
           block_data_col += kernel_w;
         }
       }
